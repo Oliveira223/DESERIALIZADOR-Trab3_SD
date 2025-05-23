8dc92a9db99aafaba419b5b7bfb7d12d585f3d59
Pedro Oliveira <162735266+Oliveira223@users.noreply.github.com>
2025-05-23 12:27:09 +0000

    starting

--- HDL/deserializer.sv
+++ HDL/deserializer.sv
@@ -1,17 +1,17 @@
 module deserializer(
     input  logic      clock,
     input  logic      reset,
-    input  logic      data_in,
+    input  logic      data_in,      //entrada de bits
     input  logic      write_in,
     output logic      data_ready,
     output logic      status_out,
-    output logic[7:0] data_out
+    output logic[7:0] data_out      //saida pacotes de 8 bits
 );
 
 
 always_ff @(posedge clock or posedge reset) begin
     if(reset) begin
-        
+
 
 
 
