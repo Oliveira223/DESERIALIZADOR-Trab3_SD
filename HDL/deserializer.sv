957be6ff05f9371048e2df51a590c23c00a7c797
Pedro Oliveira <162735266+Oliveira223@users.noreply.github.com>
2025-05-28 21:40:25 +0000

    deserialiver changes

--- HDL/deserializer.sv
+++ HDL/deserializer.sv
@@ -1,46 +1,94 @@
-/*deserializador: 
-    - recebe uma sequência de 8 bits pelo data_in;
-	- escreve palavras de 8 bits no data_out;
-	- o sinal status_out indica se o serealizador pode receber dados;
-	- o sinal write_in indica que o dado deve ser interpretado pelo deserializador;
-	- o fio data_out possui 8 bits e é a saída de dados do deserializador;
-	- os dados estão prontos para consumo quando o sinal data_ready está alto;
-    - para confirmar o dado recebido o sinal ack_in é escrito.
-
-regras: 
-    - O serializador deve receber 1 bit pelo data_in;
-    - Se o sinal write_in estiver alto, o bit recebido deverá ser guardado; 
-    - Quando houver 8 bits guardados, o sinal data_ready deverá estar alto e os bits guardados deverão aparecer em data_out, ou seja, existem dados para transmitir;
-    - Os valores de data_out e data_ready deverão se manter até o sinal ack_in ficar alto, ou seja, que os dados foram recebidos;
-    - Enquanto o deserializador não conseguir enviar os dados, deverá manter o sinal de status_out alto, ou seja, está ocupado;
-    - Este módulo deverá funcionar a 100KHz. 
-*/
-
 module deserializer(
-    input  logic      clock,
-    input  logic      reset,
+    input  logic clock_100,
+    input  logic reset,
 
-    input  logic [7:0] data_in,      //entrada de 8 bits
-    input  logic      write_in,
+    input  logic data_in,           //entrada de bit em bit
+    input  logic write_in,
+    input  logic ack_in,
 
-    output logic      data_ready,
-    output logic      status_out,
-    output logic[7:0] data_out      //saida pacotes de 8 bits
+    output logic [7:0] data_out,      //saida de 8 bits
+    output logic data_ready
 );
 
+    logic status_out;
+
+typedef enum logic [1:0] {
+    IDLE,
+    RECEIVING,
+    READY
+  } state_t;
+
+  state_t state;
+
+  logic [3:0] count;        //Conta bits até 8
+  logic [7:0] tmp_vector;   //Vetor temporario que enviara os dados completos para data_out em seguida, para evitar problemas
+
+    always_ff @(posedge clock_100 or posedge reset) begin
+        if(reset) begin
+          state <= IDLE;
+          tmp_vector <= 8'b0;
+          count <= 4'b0;
+          data_out <= 8'b0;
+          data_ready <= 0;
+          status_out <= 0;
+        end else begin
+            case(state)
+
+                //Estado parado, esperando o write_in
+                IDLE: begin
+                    $display("Entrei no estado IDLE");
+                    data_ready <= 0;
+                    status_out <= 0;
+
+                    //tem que ter a logica igual aqui dentro para não perder o primeiro bit (por causa do clock), ou seja, essa logica abaixo só acontece uma vez (salvando o primeiro bit).
+                    if(write_in) begin
+                        tmp_vector <= {tmp_vector[6:0], data_in}; //tmp_vector[6:0] faz o shift left e então recebe o novo bit
+                        count <= count + 1;
+                        state <= RECEIVING;
+                    end
+                end
+
+                RECEIVING: begin
+                    $display("Entrei no estado RECEIVING");
+                    status_out <= 1; //ocupado recebendo dados
+                    //agora pega os demais bits
+                    if(write_in) begin
+                        tmp_vector <= {tmp_vector[6:0], data_in}; 
+                        count <= count + 1;
+                    end
 
 
-always_ff @(posedge clock or posedge reset) begin
-    if(reset) begin
-        data_in <= 0;
-        data_out <= 0;
+                    //quando chegar no 7° ele vai fazer mais um shift para evitar perder dados, porém diretamente no data_out 
+                    if(count == 7 && write_in) begin
+                        data_out <= {tmp_vector[6:0], data_in}; 
+                        data_ready <= 1;   //to pronto pra ir pra fila
+                        $display("Cheguei em 8 bits");
+                        state <= READY;
+                    end
+
+
+
+                end
+
+                READY: begin
+                    $display("Entrei no estado READY");
+                    if(ack_in) begin
+                        data_ready <= 0;
+                        status_out <= 0;
+                        tmp_vector <= 8'b0;
+                        count <= 0;
+                        data_out <= 8'b0;
+                        state <= IDLE;
+                    end
+                end
+
+                default: state <= IDLE;
+
+            endcase
+
         
-    end else begin
 
+        end
     end
-
-
-
-
 
 endmodule
