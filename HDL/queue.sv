380c249c05b0139ec3ee3bf2a6a07d604b96ee5c
Bernardo F. Fraga <bfoquesdefraga@gmail.com>
2025-05-30 12:26:36 +0000

    added states in queue

--- HDL/queue.sv
+++ HDL/queue.sv
@@ -1,21 +1,25 @@
-/*fila:
+/*
+
+fila:
         - é um container limitado do tipo LIFO de 8 bits;
         - elementos são inserdos através do data_in e enqueue_in;
         - elementos são removidos através do data_out e dequeue_in;
         - o sinal len_out de 8 bits que indica o nº de elementos da fila;
         - quando o sinal dequeue_in sobe, o 1º dado a ser retirado deve aparecer em data_out no próximo ciclo SE o nº de elementos (len_out) for maior que 0.
+
 regras: 
-             - A fila possui um tamanho fixo de 8 espaços, cada com 8 bits;
-             - O sinal len_out deve informar o número de espaços utilizados;
-             - Para colocar um elemento na fila, o elemento deverá aparecer no sinal data_in e o sinal enqueue_in deverá estar alto; 
-             - Para remover um elemento da fila, o sinal dequeue_in deve ser levantado e, no ciclo subsequente, o dado removido deverá aparecer no sinal data_out; 
-             - Este módulo deverá funcionar a 10KHz.
+        - A fila possui um tamanho fixo de 8 espaços, cada com 8 bits;
+        - O sinal len_out deve informar o número de espaços utilizados;
+        - Para colocar um elemento na fila, o elemento deverá aparecer no sinal data_in e o sinal enqueue_in deverá estar alto; 
+        - Para remover um elemento da fila, o sinal dequeue_in deve ser levantado e, no ciclo subsequente, o dado removido deverá aparecer no sinal data_out; 
+        - Este módulo deverá funcionar a 10KHz.
+
 
 */
 
 
 module queue(
-    input logic clock,
+    input logic clock_10, // clock 10KHZ
     input logic reset,
 
     input logic data_in,
@@ -26,8 +30,7 @@ module queue(
     output logic len_out
 );
 
-//estados: pronto (para receber)
-//         salvando 
+typedef enum logic [1:0] {ENQ, DEQ, FULL}
 
 
 always @(posedge clock or posedge reset) begin
@@ -38,7 +41,8 @@ always @(posedge clock or posedge reset) begin
     data_out    <= 0;
     len_out     <= 0;
     end else begin
-
+           
+            
 
 
 
